bff73b0063da0c380d50b32c8b60fe1168a48aca
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2024-01-10 12:59:34 +0300

    sorted function add

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,22 +1,40 @@
-import { healthLine } from '../js/app.js';
+import { healthLine, usersSorted } from "../js/app.js";
 
-test('healthLine test full', () => {
-  const list = { name: 'Маг', health: 90 };
+test("healthLine test full", () => {
+  const list = { name: "Маг", health: 90 };
   const result = healthLine(list);
 
-  expect(result).toBe('healthy');
+  expect(result).toBe("healthy");
 });
 
-test('healthLine test wounded', () => {
-  const list = { name: 'Маг', health: 49 };
+test("healthLine test wounded", () => {
+  const list = { name: "Маг", health: 49 };
   const result = healthLine(list);
 
-  expect(result).toBe('wounded');
+  expect(result).toBe("wounded");
 });
 
-test('healthLine test critical', () => {
-  const list = { name: 'Маг', health: 14 };
+test("healthLine test critical", () => {
+  const list = { name: "Маг", health: 14 };
   const result = healthLine(list);
 
-  expect(result).toBe('critical');
+  expect(result).toBe("critical");
+});
+
+test("sorted users health", () => {
+  const users = [
+    { name: "мечник", health: 10 },
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+  ];
+
+  const usersSortedArray = [
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+    { name: "мечник", health: 10 },
+  ];
+
+  const result = usersSorted(users);
+
+  expect(result).toEqual(usersSortedArray);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
 export function healthLine(item) {
   if (item.health >= 50) {
-    return 'healthy';
+    return "healthy";
   }
   if (item.health < 50 && item.health >= 15) {
-    return 'wounded';
+    return "wounded";
   }
 
-  return 'critical';
+  return "critical";
+}
+
+export function usersSorted(users) {
+  users.sort((a, b) => {
+    if (a.health > b.health) {
+      return 1;
+    }
+    return 0;
+  });
 }
